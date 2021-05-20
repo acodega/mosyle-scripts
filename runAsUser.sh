381f0c1312725b945aa27c16863b4adbc2a93427
Adam Codega <acodega@users.noreply.github.com>
2021-05-20 15:18:37 -0400

    Create runAsUser.sh

---
+++ runAsUser.sh
@@ -0,0 +1,15 @@
+currentUser=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ { print $3 }')
+uid=$(id -u "$currentUser")
+
+runAsUser() {  
+  if [ "$currentUser" != "loginwindow" ]; then
+    launchctl asuser "$uid" sudo -u "$currentUser" "$@"
+  else
+    echo "No user logged in."
+    # uncomment the exit command
+    # to make the function exit with an error when no user is logged in
+    # exit 1
+  fi
+}
+
+runAsUser defaults write com.apple.finder FXRemoveOldTrashItems -bool TRUE
