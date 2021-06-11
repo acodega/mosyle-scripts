a8f1272043b83fe6219806b2a235931b2f0281f2
Adam Codega <acodega@users.noreply.github.com>
2021-06-11 10:39:48 -0400

    Changed to arrays and loops

--- FileCheckCA.sh
+++ FileCheckCA.sh
@@ -1,36 +1,25 @@
 #!/bin/bash
 
 #
-# File Name: FileCheckCA.sh https://github.com/acodega/mosyle
+# File Name: FileCheck.sh https://github.com/acodega/mosyle
 # Custom command attribute to check and report if a file or files exists
 # Custom command attributes are intended for use with Mosyle MDM
 # Remember, we want to produce output if the file is found or not found
 # This example was used to check for leftover FileWave remnants
 #
 
-path1=/var/FileWave/
-path2=/usr/local/sbin/FileWave.app
-path3=/sbin/fwcontrol
-path4=/Applications/FileWave\ Kiosk.app
-path5=/System/Library/StartupItems/FWClient/
+paths=(
+  "/var/FileWave/"
+  "/usr/local/sbin/FileWave.app"
+  "/sbin/fwcontrol"
+  "/Applications/FileWave Kiosk.app"
+  "/System/Library/StartupItems/FWClient/"
+)
 
-
-if [ -e "$path1" ]; then echo "Yes: '$path1'"
-else echo "Not found: '$path1'"
-fi
-
-if [ -e "$path2" ]; then echo "Yes: '$path2'"
-else echo "Not found: '$path2'"
-fi
-
-if [ -e "$path3" ]; then echo "Yes: '$path3'"
-else echo "Not found: '$path3'"
-fi
-
-if [ -e "$path4" ]; then echo "Yes: '$path4'"
-else echo "Not found: '$path4'"
-fi
-
-if [ -e "$path5" ]; then echo "Yes: '$path5'"
-else echo "Not found: '$path5'"
-fi
+for path in "${paths[@]}"; do
+  if [ ! -e "${path}" ]; then
+    echo "Not found: '${path}'"
+  else
+    echo "Found: '${path}'" 
+  fi
+done
