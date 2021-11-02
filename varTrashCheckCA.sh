4e4de34583e33dc65b66d56406899ba85fb886d6
Adam Codega <acodega@users.noreply.github.com>
2021-11-02 09:15:20 -0400

    Create varTrashCheckCA.sh

---
+++ varTrashCheckCA.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#
+# File Name: varTrashCheckCA.sh https://github.com/acodega/mosyle
+# Custom command attribute to check and report on the size of the /private/var/root/.Trash folder
+# Adobe and Microsoft OneDrive leave old versions here without deleting them
+# Custom command attributes are intended for use with Mosyle MDM
+#
+
+echo $(du -sh /private/var/root/.Trash)
