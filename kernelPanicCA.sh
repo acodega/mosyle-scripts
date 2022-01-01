b7279b268ac4e99a09cf6160b20e5553275e8677
hkystar35 <32749858+hkystar35@users.noreply.github.com>
2021-12-31 22:45:27 -0700

    changed days

--- kernelPanicCA.sh
+++ kernelPanicCA.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-# Custom command attribute check for kernel panics which occurred in the last seven days.
+# Custom command attribute check for kernel panics which occurred in the last forteen days.
 # Displays the number of occurences.
 #
 #
 
-PanicLogCount=$(/usr/bin/find /Library/Logs/DiagnosticReports -Btime -7 -name *.panic | grep . -c)
+PanicLogCount=$(/usr/bin/find /Library/Logs/DiagnosticReports -Btime -14 -name *.panic | grep . -c)
 
 /bin/echo "$PanicLogCount"
