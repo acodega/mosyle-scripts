cc781f27f5be4fb63b40501d01afe9106ce7631f
Adam Codega <acodega@users.noreply.github.com>
2022-02-15 21:32:15 -0500

    Update with battery check
    Checks if computer is a laptop, and would not have a battery.

--- BatteryHealthCA.sh
+++ BatteryHealthCA.sh
@@ -5,11 +5,17 @@
 #
 # Custom command attribute to check and report the Mac's battery health and cycle count
 # Output example: "Normal (20)" indicates battery health reported normal with a cycle count of 20
+# Includes logic to echo "N/A" if the computer does not have a battery
 # 
 # Custom command attributes are intended for use with Mosyle MDM
 #
 
-batteryCondition=$(system_profiler SPPowerDataType | grep "Condition" | awk -F': ' '/Condition:/{ print $NF }')
-batteryCycleCount=$(system_profiler SPPowerDataType | grep "Cycle Count" | awk '{print $3}')
-
-echo $batteryCondition \($batteryCycleCount\)
+hwType=$(/usr/sbin/system_profiler SPHardwareDataType | grep "Model Identifier" | grep "Book")	
+if [ "$hwType" != "" ];
+  then
+    batteryCondition=$(system_profiler SPPowerDataType | grep "Condition" | awk -F': ' '/Condition:/{ print $NF }')
+    batteryCycleCount=$(system_profiler SPPowerDataType | grep "Cycle Count" | awk '{print $3}')
+    echo $batteryCondition \($batteryCycleCount\)
+  else
+	  echo "N/A"
+fi
