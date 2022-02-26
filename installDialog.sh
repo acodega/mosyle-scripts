4cb7c95a7ac74b936141283b6df64e9bcb1c9025
Adam Codega <acodega@users.noreply.github.com>
2022-02-26 08:03:40 -0500

    Added AppleScript fallback
    Display an AppleScript error if something went wrong, helpful when you need Dialog installed during computer deployment.

--- installDialog.sh
+++ installDialog.sh
@@ -6,30 +6,47 @@ url=$(curl --silent --fail "https://api.github.com/repos/bartreardon/Dialog/rele
 expectedTeamID="PWA5E9TQ59"
 exitCode=0
 
-# check for Dialog and install if not found
+# If something goes wrong we want to notify the user using AppleScript and exit the script
+# The second EndOfScript *must* be tabbed, do not uses spaces.
+displayDialog() {
+  message="A problem was encounted setting up this Mac. Please contact IT."
+  currentUser=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ { print $3 }')
+  if [[ "$currentUser" != "" ]]; then
+    uid=$(id -u "$currentUser")
+    launchctl asuser "$uid" /usr/bin/osascript <<-EndOfScript
+      button returned of ¬
+      (display dialog "$message" ¬
+      buttons {"OK"} ¬
+      default button "OK")
+		EndOfScript
+  fi
+}
+
+# Check for Dialog and install if not found
 if [ ! -e "/Library/Application Support/Dialog/Dialog.app" ]; then
   echo "Dialog not found. Installing."
-  # create temporary working directory
+  # Create temporary working directory
   workDirectory=$( /usr/bin/basename "$0" )
   tempDirectory=$( /usr/bin/mktemp -d "/private/tmp/$workDirectory.XXXXXX" )
   echo "Created working directory '$tempDirectory'"
-  # download the installer package
+  # Download the installer package
   echo "Downloading Dialog package"
   /usr/bin/curl --location --silent "$url" -o "$tempDirectory/Dialog.pkg"
-  # verify the download
+  # Verify the download
   teamID=$(/usr/sbin/spctl -a -vv -t install "$tempDirectory/Dialog.pkg" 2>&1 | awk '/origin=/ {print $NF }' | tr -d '()')
   echo "Team ID for downloaded package: $teamID"
-  # install the package if Team ID validates
+  # Install the package if Team ID validates
   if [ "$expectedTeamID" = "$teamID" ] || [ "$expectedTeamID" = "" ]; then
     echo "Package verified. Installing package Dialog.pkg"
     /usr/sbin/installer -pkg "$tempDirectory/Dialog.pkg" -target /
     exitCode=0
   else 
     echo "Package verification failed before package installation could start. Download link may be invalid. Aborting."
+    displayDialog
     exitCode=1
     exit $exitCode
   fi
-  # remove the temporary working directory when done
+  # Remove the temporary working directory when done
   echo "Deleting working directory '$tempDirectory' and its contents"
   /bin/rm -Rf "$tempDirectory"  
 else echo "Dialog already found. Proceeding..."
